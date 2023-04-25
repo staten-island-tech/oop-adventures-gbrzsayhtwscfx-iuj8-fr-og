a7f160ca4b2c3ab706b26c18998cecffc97f8f90
AnthonyManzur <124072131+AnthonyManzur@users.noreply.github.com>
2023-04-25 10:47:59 -0400

    Update NPC.py

---
+++ NPC.py
@@ -0,0 +1,14 @@
+
+
+import random 
+numnpc = [3, 4, 5]
+rnpc = random.choice(numnpc)
+if rnpc == 3:
+    print("3 npcs")
+if rnpc == 4:
+    print("4 npcs")
+
+
+
+""" if rnumber = 1
+    def function1(): """
