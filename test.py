75cadbfa9de2df7be105072f9ff8ec1737102b71
LKWQiu <124072151+LKWQiu@users.noreply.github.com>
2023-04-26 10:54:24 -0400

    Update test.py

--- test.py
+++ test.py
@@ -1,6 +1,7 @@
-
+import random
 damage = 8
-health = 1000
+health = 400
+rroll_ = 1
 class Player:
     def __init__(self, id, name, health, damage ):
         self.id = id
@@ -11,16 +12,30 @@ class Player:
 
         
 v=["Blindseer", "Dawnwalker", "Starkindred", "Contractor", "Visionshaper", "Silentheart", "Oathless", "Linkstrider", "Jetstriker", "Arcwarder"]
-Rapier_ = ["Aprentice Rapier", "Ironstinger", "Crucible Rapier", "Krystreza"]
+Rapier_ = ["Apprentice Rapier", "Ironstinger", "Crucible Rapier", "Krystreza"]
 Dagger_ = ["Canor Fang", "Tanto"]
 
 name = input("What is your name? ")
 y = input("What is your weapon class?(Light, Medium, Heavy) ").capitalize()
-if y == "Light":
+while y == "Light":
     Lweapon = input("Dagger, Rapier, or Fist? ").capitalize()
     if Lweapon == "Rapier":
-        Rapier1 = input("What type of Rapier? ").capitalize()
-    elif Lweapon == "Dagger":
+        lightwep = random.choice(Rapier_)
+        print(lightwep)
+        rroll = input("Do you wish to reroll Rapier? y/n ")
+        if rroll == "y".capitalize:
+            rroll_ = rroll_ + 1
+            print(lightwep)
+            if rroll_ == 3:
+                print("Max rolls reached(3/3) ")
+                break
+        if rroll == "n":
+            print(f"Rolls used: {rroll_}")
+            break
+            
+
+
+    if Lweapon == "Dagger":
         Dagger1 = input("What type of Dagger? ").capitalize()
 if y == "Heavy":
     Hweapon = input("Ghammer, Gsword, or Gaxe? ").capitalize()
@@ -36,14 +51,26 @@ class Light(Player):
         self.Dagger = Dagger
         self.Fist = Fist
 def lightdmg_():
-    if Rapier1 in Rapier_:
-        if Rapier1 == "Ironstinger":
+    if "lightwep" in Rapier_:
+        if lightwep == "Ironstinger":
             Ironstinger = (f"Damage: {damage + 10.8}")
             print (Ironstinger)
-        if Rapier1 == "Kyrstreeza":
+            print(lightwep)
+        if lightwep == "Kyrstreeza":
             Kyrstreeza = (f"Damage: {damage + 24.5}")
-            print (Kyrstreeza)   
-    if Dagger1 in Dagger_:
+            print (Kyrstreeza)  
+            print(lightwep) 
+        if lightwep == "Crucible Rapier":
+            Crucible_Rapier = (f"Damage: {damage + 20.6}")
+            print (Crucible_Rapier)
+            print(lightwep)
+        if lightwep == "Apprentice Rapier":
+            Apprentice_Rapier = (f"Damage: {damage + 15.9}")
+            print (Apprentice_Rapier)
+            print(lightwep)
+
+
+    if "Dagger1" in Dagger_:
         if Dagger1 == "Canor Fang":
             Canor_Fang = (f"Damage: {damage + 15.2}")
             print (Canor_Fang)
@@ -51,8 +78,9 @@ def lightdmg_():
             Tanto = (f"Damage: {damage + 20.5}")
             print (Tanto)
 lightdmg_()
+
 def LightStat():
-    Lucas=Light( name, health, damage, Rapier_, "Apprentice Rapier", "Flamekeeper Cestus")
+    Lucas=Light( name, health, damage, Rapier_, "Iron cestus", Dagger_)
     print(f"Name:  {Lucas.name}")
     print(f"Health: {Lucas.health}")
     lightdmg_()
