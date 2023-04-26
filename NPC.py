0a3db57ef3417dca5f888a736faf5d0de2eee6ae
AnthonyManzur <124072131+AnthonyManzur@users.noreply.github.com>
2023-04-26 10:53:55 -0400

    4/26
    4/26

--- NPC.py
+++ NPC.py
@@ -1,12 +1,26 @@
-
+numnpcnames = ["Jeff", "Lucas", "Anthony", "Raymond", "Matthew", "Ethan", "Omar"]
+rnumnpcnames = random.choice(numnpcnames)
 
 import random 
 numnpc = [3, 4, 5]
 rnpc = random.choice(numnpc)
 if rnpc == 3:
-    print("3 npcs")
+    print("3 enemies has spawned")
+    
+ 
+    numattack = [1, 2, 3, 4]
+    rattack = random.choice(numattack)
+    if rattack == 1:
+        print("You ")        
+    if rattack == 2:
+        print("")
+    if rattack == 3:
+        print("")
+    if rattack == 4:
+        print("")
 if rnpc == 4:
     print("4 npcs")
+
 if rnpc == 5:
     print("5 npc")
 
