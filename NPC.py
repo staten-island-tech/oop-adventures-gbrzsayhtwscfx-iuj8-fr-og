731df537b34ddf3448c731c0dec7fd374c3b450f
AnthonyManzur <124072131+AnthonyManzur@users.noreply.github.com>
2023-04-27 11:00:44 -0400

    4/27
    4/27

--- NPC.py
+++ NPC.py
@@ -1,13 +1,23 @@
+import npc2
+
 numnpcnames = ["Jeff", "Lucas", "Anthony", "Raymond", "Matthew", "Ethan", "Omar"]
 rnumnpcnames = random.choice(numnpcnames)
+
+HP = 250
+
+npcdamage = [250 -attack]
+
+
 
 import random 
 numnpc = [3, 4, 5]
 rnpc = random.choice(numnpc)
 if rnpc == 3:
     print("3 enemies has spawned")
+    print(HP)
+
     
- 
+
     numattack = [1, 2, 3, 4]
     rattack = random.choice(numattack)
     if rattack == 1:
@@ -18,11 +28,47 @@ if rnpc == 3:
         print("")
     if rattack == 4:
         print("")
+
+    if HP <= 0:
+        print(f"you have defeated {rnumnpcnames}")
+        print(f"You are now fighting {rnumnpcnames}")
+
+         numattack = [1, 2, 3, 4]
+        rattack = random.choice(numattack)
+        if rattack == 1:
+            print("You ")        
+        if rattack == 2:
+            print("")
+        if rattack == 3:
+            print("")
+        if rattack == 4:
+            print("")
+
 if rnpc == 4:
-    print("4 npcs")
+    print("4 enemies have spawned")
+    numattack1 = [1, 2, 3, 4]
+    rattack1 = random.choice(numattack)
+    if rattack1 == 1:
+        print("You ")        
+    if rattack1 == 2:
+        print("")
+    if rattack1 == 3:
+        print("")
+    if rattack1 == 4:
+        print("")
 
 if rnpc == 5:
-    print("5 npc")
+    print("5 enemies have spawned")
+    numattack2 = [1, 2, 3, 4]
+    rattack2 = random.choice(numattack)
+    if rattack2 == 1:
+        print("You ")        
+    if rattack2 == 2:
+        print("")
+    if rattack2 == 3:
+        print("")
+    if rattack2 == 4:
+        print("")
 
 
 """ if rnumber = 1
