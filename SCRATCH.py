e5ecbe6cb765355ff93519a75624cec2bf0286dc
LKWQiu <124072151+LKWQiu@users.noreply.github.com>
2023-05-02 10:58:11 -0400

    Update SCRATCH.py

--- SCRATCH.py
+++ SCRATCH.py
@@ -24,8 +24,7 @@ class Player:
 
 class Weapon(Player):
     Chance = [1,2,3,4,5,6,7,8,9,10]
-    Rapier_ = ["Aprentice Rapier", "Ironstinger", "Crucible Rapier", "Krystreeza"]
-    
+
     def __init__(self, name, health, damage, rroll, weptype, lightwep):
         super(). __init__(self, name, health, damage, rroll, weptype)
         self.lightwep = lightwep
@@ -34,13 +33,24 @@ class Weapon(Player):
         Ironstinger_ = [1,2,3,4]
         Aprentice_Rapier_ = [5,6,7]
         Crucible_Rapier = [8,9]
-        Krystreeza_ = [10]
+        Krystreza_ = [10]
+
+        Flareblood_Kamas = [1]
+        Canor_Fang_ = [2,3,4]
+        Nemits_Sickle_ = [5,6,7]
+        Tanto = [8,9]
+        Krulian_Knife = [10]
+
+        Iron_Cestus = [1,2,3,4,5]
+        Legion_Cestus = [6,7,8]
+        Flamekeeper_Cestus = [9,10]
+
         weapon_ = input("What is your weapon class?(Light, Medium, Heavy) ").capitalize()
         while weapon_ == "Light":
             Lweapon = input("Dagger, Rapier, or Fist? ").capitalize()
+            chance_ = random.choice(self.Chance)
+            print(chance_)
             if Lweapon == "Rapier":
-                chance_ = random.choice(self.Chance)
-                print(chance_)
                 if chance_ in Ironstinger_:
                     self.Stats.append("Weapon: Ironstinger")
                     break
@@ -50,11 +60,43 @@ class Weapon(Player):
                 if  chance_ in Crucible_Rapier:
                     self.Stats.append("Weapon: Crucible Rapier")
                     break
-                if chance_ in Krystreeza_:
-                    self.Stats.append("Weapon: Krystreeza")
+                if chance_ in Krystreza_:
+                    self.Stats.append("Weapon: Krystreza")
                     break
-            if weapon_ == "Medium" and "Heavy":
+            if Lweapon == "Dagger":
+                if chance_ in Canor_Fang_:
+                    self.Stats.append("weapon: Canor Fang")
+                    break
+                if chance_ in Nemits_Sickle_:
+                    self.Stats.append("Weapon: Nemits Sickle")
+                    break
+                if chance_ in Tanto:
+                    self.Stats.append("Weapon: Tanto")
+                    break
+                if chance_ in Krulian_Knife:
+                    self.Stats.append("Weapon: Krulian Knife")
+                    break
+                if chance_ in Flareblood_Kamas:
+                    self.Stats.append("Weapon: Flareblood Kamas")
+                    break
+            if Lweapon == "Fist":
+                if chance_ in Iron_Cestus:
+                    self.Stats.append("Weapon: Iron Cestus")
+                    break
+                if chance_ in Legion_Cestus:
+                    self.Stats.append("Weapon: Legion Cestus")
+                    break
+                if chance_ in Flamekeeper_Cestus:
+                    self.Stats.append("Weapon: Flamekeeper Cestus")
+                    break
+            if weapon_ == "Heavy":
                 break
+        while weapon_ == "Medium":
+            Mweapon = input("Sword, Club, Spear or Rifle? ").capitalize()
+            chance_ = random.choice(self.Chance)
+            print(chance_)
+            if Mweapon == "Sword":
+                print("sword")
 
 
 
