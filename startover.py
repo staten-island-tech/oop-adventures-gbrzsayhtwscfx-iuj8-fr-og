95bed1c7150c3602e3ed86d4d065ed83c375d33c
AnthonyManzur <124072131+AnthonyManzur@users.noreply.github.com>
2023-05-02 11:00:00 -0400

    Update startover.py

--- startover.py
+++ startover.py
@@ -1,11 +1,12 @@
-import npc2
-
-numnpcnames = ["Jeff", "Lucas", "Anthony", "Raymond", "Matthew", "Ethan", "Omar"]
-rnumnpcnames = random.choice(numnpcnames)
-
 HP = 250
 
-npcdamage = [250 -attack]
+
+
+attack = 10
+
+PlayerHp = 100
+
+npcHP = [250 - attack]
 
 
 
@@ -22,8 +23,33 @@ numstart = random.choice(numstart)
 if numstart == 1:
     print("you make the first move")
 
-after your choice of attack
+# after your choice of attack
 
-    
+
 if numstart == 2:
     print("your enemy makes the first move")
+
+    numattack = [1, 2, 3, 4]
+    rattack = random.choice(numattack)
+
+    if rattack == 1:
+        PlayerHp - 10
+        print("you were punched, 10 HP")
+        print(PlayerHp)
+        PlayerAttack = input("choose your attack(1,2,3, or 4").capitalize()
+        if PlayerAttack == 1:
+            print("You have slashed your enemy")
+
+
+    if rattack == 2:
+        PlayerHp - 10
+        print("You were slashed, minus 10 HP")
+        print(PlayerHp)
+    if rattack == 3:
+        PlayerHp - 10
+        print("You were shot, minus 10 HP")
+        print(PlayerHp)
+    if rattack == 4:
+        PlayerHp - 10
+        print("You were stabbed, minus 10 HP")
+        print(PlayerHp)
