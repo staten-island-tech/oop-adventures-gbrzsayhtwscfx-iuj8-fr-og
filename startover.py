fd49fab1a2e464d3f225a87280542ad5190a132e
AnthonyManzur <124072131+AnthonyManzur@users.noreply.github.com>
2023-05-04 10:32:24 -0400

    Update startover.py

--- startover.py
+++ startover.py
@@ -23,7 +23,7 @@ numstart = random.choice(numstart)
 if numstart == 1:
     print("you make the first move")
 
-
+#not working on this now
 
 
 while numstart == 2:
@@ -32,23 +32,38 @@ while numstart == 2:
     numattack = [1, 2, 3, 4]
     rattack = random.choice(numattack)
 
+    def playerattack():
     if rattack == 1:
-        PlayerHp - 10 == PlayerHp
+        PlayerHp = 100
+        PlayerHp_ = PlayerHp - 10
         print("you were punched, 10 HP")
-        print(PlayerHp)
+        print(PlayerHp_)
         PlayerAttack = input("choose your attack(1,2,3, or 4").capitalize()
         if PlayerAttack == 1:
             print("You have slashed your enemy")
             npcHP - 10
             print(f"your enemy has {npcHP} HP")
 
+            if npcHP > 0:
+                break 
+                
+
+            if npcHP <= 0:
+                print("you have defeated your enemy")
+                break
 
         if PlayerAttack == 2:
             print("You punched your enemy")
             npcHP - 10
             print(f"your enemy has {npcHP} HP")
 
-            
+            if npcHP > 0:
+                break 
+                
+
+            if npcHP <= 0:
+                print("you have defeated your enemy")
+                break            
             
         
         if PlayerAttack == 3:
@@ -56,6 +71,14 @@ while numstart == 2:
             npcHP - 10
             print(f"your enemy has {npcHP} HP")
         
+            if npcHP > 0:
+                break 
+                
+
+            if npcHP <= 0:
+                print("you have defeated your enemy")
+                break
+
         if PlayerAttack == 4:
             print("you have kicked your enemy")
             npcHP - 10
@@ -70,29 +93,51 @@ while numstart == 2:
                 break
 
     if rattack == 2:
-        PlayerHp - 10
+        PlayerHp = 100
+        PlayerHp_ = PlayerHp - 10
         print("You were slashed, minus 10 HP")
-        print(PlayerHp)
+        print(PlayerHp_)
         PlayerAttack = input("choose your attack(1,2,3, or 4").capitalize()
         if PlayerAttack == 1:
             print("You have slashed your enemy")
             npcHP - 10
             print(f"your enemy has {npcHP} HP")
 
+            if npcHP > 0:
+                break 
+                
+
+            if npcHP <= 0:
+                print("you have defeated your enemy")
+                break
 
         if PlayerAttack == 2:
             print("You punched your enemy")
             npcHP - 10
             print(f"your enemy has {npcHP} HP")
 
-            
+            if npcHP > 0:
+                break 
+                
+
+            if npcHP <= 0:
+                print("you have defeated your enemy")
+                break            
             
         
         if PlayerAttack == 3:
             print("your have shot your enemy")
             npcHP - 10
             print(f"your enemy has {npcHP} HP")
-        
+
+            if npcHP > 0:
+                break 
+                
+
+            if npcHP <= 0:
+                print("you have defeated your enemy")
+                break    
+
         if PlayerAttack == 4:
             print("you have kicked your enemy")
             npcHP - 10
@@ -106,29 +151,51 @@ while numstart == 2:
                 print("you have defeated your enemy")
                 break
     if rattack == 3:
-        PlayerHp - 10
+        PlayerHp = 100
+        PlayerHp_ = PlayerHp - 10
         print("You were shot, minus 10 HP")
-        print(PlayerHp)
+        print(PlayerHp_)
         PlayerAttack = input("choose your attack(1,2,3, or 4").capitalize()
         if PlayerAttack == 1:
             print("You have slashed your enemy")
             npcHP - 10
             print(f"your enemy has {npcHP} HP")
 
+            if npcHP > 0:
+                break 
+                
+
+            if npcHP <= 0:
+                print("you have defeated your enemy")
+                break
 
         if PlayerAttack == 2:
             print("You punched your enemy")
             npcHP - 10
             print(f"your enemy has {npcHP} HP")
 
-            
+            if npcHP > 0:
+                break 
+                
+
+            if npcHP <= 0:
+                print("you have defeated your enemy")
+                break            
             
         
         if PlayerAttack == 3:
             print("your have shot your enemy")
             npcHP - 10
             print(f"your enemy has {npcHP} HP")
-        
+
+            if npcHP > 0:
+                break 
+                
+
+            if npcHP <= 0:
+                print("you have defeated your enemy")
+                break
+
         if PlayerAttack == 4:
             print("you have kicked your enemy")
             npcHP - 10
@@ -142,29 +209,50 @@ while numstart == 2:
                 print("you have defeated your enemy")
                 break
     if rattack == 4:
-        PlayerHp - 10
+        PlayerHp = 100
+        PlayerHp_ = PlayerHp - 10
         print("You were stabbed, minus 10 HP")
-        print(PlayerHp)
+        print(PlayerHp_)
         PlayerAttack = input("choose your attack(1,2,3, or 4").capitalize()
         if PlayerAttack == 1:
             print("You have slashed your enemy")
             npcHP - 10
             print(f"your enemy has {npcHP} HP")
 
+            if npcHP > 0:
+                break 
+                
+
+            if npcHP <= 0:
+                print("you have defeated your enemy")
+                break
 
         if PlayerAttack == 2:
             print("You punched your enemy")
             npcHP - 10
             print(f"your enemy has {npcHP} HP")
 
-            
+            if npcHP > 0:
+                break 
+                
+
+            if npcHP <= 0:
+                print("you have defeated your enemy")
+                break            
             
         
         if PlayerAttack == 3:
             print("your have shot your enemy")
             npcHP - 10
             print(f"your enemy has {npcHP} HP")
-        
+            if npcHP > 0:
+                break 
+                
+
+            if npcHP <= 0:
+                print("you have defeated your enemy")
+                break
+
         if PlayerAttack == 4:
             print("you have kicked your enemy")
             npcHP - 10
